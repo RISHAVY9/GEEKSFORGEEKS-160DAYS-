35cb0e6f384ed449ea85a94f971601511ef0e996
RISHAV YADAV <132156106+RISHAVY9@users.noreply.github.com>
2025-09-02 11:50:05 +0530

    Update DAY4(Rotate Array).java

--- DAY4(Rotate Array).java
+++ DAY4(Rotate Array).java
@@ -5,12 +5,19 @@ class Solution {
     static void rotateArr(int arr[], int d) {
         // add your code here
         int n = arr.length ;
-        for(int i = 0 ; i < d ; i ++){
-            int first = arr[0] ;
-            for(int j = 0 ; j < n -1 ; j++){
-                arr[j] = arr[j+1];
-            }
-            arr[n-1] = first ;
+        d %= n ;
+        int [] temp = new int[n];
+        for(int i = 0 ; i <n-d ; i++){
+        temp[i] = arr[d+i];
+            
         }
+        for(int i =  0 ; i <d ; i++){
+            temp [n-d+i]= arr[i];
+        }
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = temp[i];
+        }
+        
+
     }
 }
